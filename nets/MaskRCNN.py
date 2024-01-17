d680ced394809b00bb6a3669a99d5dadb3210ccf
ShansZhou <476993098@qq.com>
2024-01-17 11:43:55 +0800

    add masrcnn

---
+++ nets/MaskRCNN.py
@@ -0,0 +1,18 @@
+
+
+import torch.nn as nn
+
+
+
+class MaskRcnn(nn.Module):
+    def __init__(self):
+        super(MaskRcnn, self).__init__()
+        
+    def train():
+        pass
+    
+    def foward():
+        pass
+    
+    def loss():
+        pass
